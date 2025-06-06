d4b4d6d054d38c2feede5c18e00a727a30c7fc5f
Ahmoham98 <electricallover45@gmail.com>
2025-06-06 11:07:47 +0330

    new test to see if I am in right branch

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
           <Route
             path="/home"
             element={
-              <PrivateRoute>
+              <PrivateRoute> 
                 <HomePage />
               </PrivateRoute>
             }
